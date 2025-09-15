5a8fe6383fcfa91c4fc979829d8a9803144dbca8
ShelMarow <2665614366@qq.com>
2025-09-15 12:29:37 +0800

    提交

--- src/main/java/net/shelmarow/betterlockon/client/render/LockOnRenderer.java
+++ src/main/java/net/shelmarow/betterlockon/client/render/LockOnRenderer.java
@@ -5,7 +5,6 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.shelmarow.betterlockon.BetterLockOn;
