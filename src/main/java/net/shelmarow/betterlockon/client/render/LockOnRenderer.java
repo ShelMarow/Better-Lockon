1da379dfacf8d8c3abcd26713f349a5a2bdffc40
ShelMarow <127160852+ShelMarow@users.noreply.github.com>
2025-11-04 16:36:58 +0800

    Revert "Neo forge 1.21.1"

--- src/main/java/net/shelmarow/betterlockon/client/render/LockOnRenderer.java
+++ src/main/java/net/shelmarow/betterlockon/client/render/LockOnRenderer.java
@@ -3,9 +3,11 @@ package net.shelmarow.betterlockon.client.render;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.shelmarow.betterlockon.BetterLockOn;
 import net.shelmarow.betterlockon.client.render.type.DefaultType;
 import net.shelmarow.betterlockon.client.render.type.IconType;
 import net.shelmarow.betterlockon.client.render.type.RPGType1;
@@ -38,10 +40,7 @@ public class LockOnRenderer extends EntityUI {
 
     @Override
     public void draw(LivingEntity entity, @Nullable LivingEntityPatch<?> entitypatch, LocalPlayerPatch playerpatch, PoseStack poseStack, MultiBufferSource buffers, float partialTicks) {
-        poseStack.pushPose();
-
-        float offsetY =  entity.getBbHeight() * 2 / 3;
-        EntityUI.setupPoseStack(poseStack, entity, 0.0F, offsetY, 0.0F, true, partialTicks);
+        Matrix4f matrix = getModelViewMatrixAlignedToCamera(poseStack, entity, 0.0F,  entity.getBbHeight() * 2 / 3, 0.0F, true, partialTicks);
 
         // 从配置获取基础大小
         float baseSize = (float) LockOnConfig.LOCK_ON_ICON_SIZE.get().doubleValue();
@@ -55,10 +54,11 @@ public class LockOnRenderer extends EntityUI {
         float b = (float) LockOnConfig.LOCK_ON_BLUE.get().doubleValue();
         float a = (float) LockOnConfig.LOCK_ON_ALPHA.get().doubleValue();
 
-        Matrix4f matrix = poseStack.last().pose();
-        renderLockOn(buffers, matrix, size / 2, -size / 2, r, g, b, a);
-
-        poseStack.popPose();
+        renderLockOn(
+                buffers, matrix,
+                size/2, -size/2,
+                r, g, b, a
+        );
     }
 
     /**
@@ -93,22 +93,24 @@ public class LockOnRenderer extends EntityUI {
 
     private void renderLockOn(MultiBufferSource buffers, Matrix4f matrix, float max, float min,float r, float g, float b, float alpha) {
 
-        IconType iconType = switch (LockOnConfig.LOCK_ON_ICON_TYPES.get()){
-            case RPG_TYPE1 -> new RPGType1();
-            default-> new DefaultType();
-        };
+
+        IconType iconType;
+        switch (LockOnConfig.LOCK_ON_ICON_TYPES.get()){
+            case RPG_TYPE1 -> iconType = new RPGType1();
+            default-> iconType = new DefaultType();
+        }
 
         //渲染背景
         VertexConsumer vc = buffers.getBuffer(LockOnRenderTypes.getLockOnQuads(iconType.getBackground()));
-        vc.addVertex(matrix, min, min, 0).setUv(0, 1).setColor(r, g, b, alpha);
-        vc.addVertex(matrix, max, min, 0).setUv(1, 1).setColor(r, g, b, alpha);
-        vc.addVertex(matrix, max, max, 0).setUv(1, 0).setColor(r, g, b, alpha);
-        vc.addVertex(matrix, min, max, 0).setUv(0, 0).setColor(r, g, b, alpha);
+        vc.vertex(matrix, min, min, 0).uv(0, 1).color(r, g, b, alpha).endVertex();
+        vc.vertex(matrix, max, min, 0).uv(1, 1).color(r, g, b, alpha).endVertex();
+        vc.vertex(matrix, max, max, 0).uv(1, 0).color(r, g, b, alpha).endVertex();
+        vc.vertex(matrix, min, max, 0).uv(0, 0).color(r, g, b, alpha).endVertex();
 
         //渲染血量（根据类型内的角度）
         int segments = 100;
         vc = buffers.getBuffer(LockOnRenderTypes.getLockOnTriangleFan(iconType.getHealth()));
-        vc.addVertex(matrix, 0, 0, 0).setUv(0.5F, 0.5F).setColor(r, g, b, alpha);
+        vc.vertex(matrix, 0, 0, 0).uv(0.5F, 0.5F).color(r, g, b, alpha).endVertex();
         for (int i = 0; i <= segments; i++) {
             float angle = (float) (iconType.getHealthStartAngle() - healthRatio * iconType.getHealthTotalAngle() * i / segments);
             float cos = (float) Math.cos(angle);
@@ -118,12 +120,12 @@ public class LockOnRenderer extends EntityUI {
             float u = 0.5F + cos * 0.5F;
             float v = 0.5F - sin * 0.5F;
 
-            vc.addVertex(matrix, x, y, 0.0F).setUv(u, v).setColor(r, g, b, alpha);
+            vc.vertex(matrix, x, y, 0.0F).uv(u, v).color(r, g, b, alpha).endVertex();
         }
 
         //渲染耐力（半圆）
         vc = buffers.getBuffer(LockOnRenderTypes.getLockOnTriangleFan(iconType.getStamina()));
-        vc.addVertex(matrix, 0, 0, 0).setUv(0.5F,0.5F).setColor(r, g, b, alpha);
+        vc.vertex(matrix, 0, 0, 0).uv(0.5F,0.5F).color(r, g, b, alpha).endVertex();
 
         for (int i = 0; i <= segments; i++) {
             float sweep = (float) (Math.PI * staminaRatio);
@@ -138,14 +140,14 @@ public class LockOnRenderer extends EntityUI {
             float u = 0.5F + cos * 0.5F;
             float v = 0.5F - sin * 0.5F;
 
-            vc.addVertex(matrix, x, y, 0.0F).setUv(u, v).setColor(r, g, b, alpha);
+            vc.vertex(matrix, x, y, 0.0F).uv(u, v).color(r, g, b, alpha).endVertex();
         }
 
         //渲染遮罩
         vc = buffers.getBuffer(LockOnRenderTypes.getLockOnQuads(iconType.getOverlay()));
-        vc.addVertex(matrix, min, min, 0).setUv(0, 1).setColor(r, g, b, alpha);
-        vc.addVertex(matrix, max, min, 0).setUv(1, 1).setColor(r, g, b, alpha);
-        vc.addVertex(matrix, max, max, 0).setUv(1, 0).setColor(r, g, b, alpha);
-        vc.addVertex(matrix, min, max, 0).setUv(0, 0).setColor(r, g, b, alpha);
+        vc.vertex(matrix, min, min, 0).uv(0, 1).color(r, g, b, alpha).endVertex();
+        vc.vertex(matrix, max, min, 0).uv(1, 1).color(r, g, b, alpha).endVertex();
+        vc.vertex(matrix, max, max, 0).uv(1, 0).color(r, g, b, alpha).endVertex();
+        vc.vertex(matrix, min, max, 0).uv(0, 0).color(r, g, b, alpha).endVertex();
     }
 }
